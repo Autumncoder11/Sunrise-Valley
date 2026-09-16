38a4916a668429b066eca0d435a0aba3aff5f43d
Autumncoder11 <autumncoder1186@gmail.com>
2026-09-16 21:05:50 +0530

    Update plot-popup.js

--- plot-popup.js
+++ plot-popup.js
@@ -348,7 +348,7 @@
   // scale factor to each kml_label_* hotspot, left at 1.0 (untouched) on
   // desktop/tablet-and-up widths.
   var MOBILE_LABEL_MAX_WIDTH_PX = 768;
-  var MOBILE_LABEL_SCALE = 0.8; // tweak this until it looks right on your phone
+  var MOBILE_LABEL_SCALE = 0.6; // tweak this until it looks right on your phone
 
   function isMobileLabelViewport() {
     return window.innerWidth <= MOBILE_LABEL_MAX_WIDTH_PX;
